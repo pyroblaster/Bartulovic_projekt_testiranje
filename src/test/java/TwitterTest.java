383b6754a915a7bcf20f9803ee43ba733c04cdd3
pyroblaster <abartulovic@etfos.hr>
2019-01-21 23:13:46 +0100

    now

--- src/test/java/TwitterTest.java
+++ src/test/java/TwitterTest.java
@@ -11,7 +11,6 @@ public class TwitterTest {
     public WebDriver webDriver;
 
     public String twitterURL = "http://www.twitter.com";
-    public String ELONMUSK_URL = "https://twitter.com/elonmusk";
 
     public String username = "***";
     public String password = "***";
@@ -48,18 +47,19 @@ public void setupTest() {
 
     @Test
     public void testTweet() throws InterruptedException {
-            testLogin(); // logiranje
+        testLogin(); // prijava
 
         WebElement tweetTextBox = webDriver.findElement((By.xpath("/html//div[@id='tweet-box-home-timeline']")));
         tweetTextBox.sendKeys("Alea iacta est!" + " " + random); //upisivanje teksta
         WebElement sendTweetBox = webDriver.findElement((By.xpath("/html//div[@id='timeline']//form[@action='//upload.twitter.com/i/tweet/create_with_media.iframe']//div[@class='TweetBoxToolbar-tweetButton tweet-button']/button[@type='button']/span[@class='button-text tweeting-text']")));
         sendTweetBox.click(); //slanje teksta
         Thread.sleep(7500);
+        webDriver.quit();
     }
 
     @Test
     public void testPoll() throws  InterruptedException{
-        testLogin();
+        testLogin(); // prijava
 
         WebElement tweetTextBox = webDriver.findElement((By.xpath("/html//div[@id='tweet-box-home-timeline']")));
         tweetTextBox.click();
@@ -83,7 +83,7 @@ public void setupTest() {
 
     @Test
     public void testLogout() throws  InterruptedException {
-        testLogin(); // logiranje
+        testLogin(); // prijava
 
         WebElement userSettings = webDriver.findElement(By.xpath("/html//a[@id='user-dropdown-toggle']"));
         userSettings.click(); //ulazak na postavke racuna
@@ -96,18 +96,18 @@ public void setupTest() {
 
     @Test
     public void testSearch() throws  InterruptedException {
-        testLogin(); // logiranje
+        testLogin(); // prijava
 
         WebElement searchBar = webDriver.findElement(By.xpath("/html//input[@id='search-query']"));
         searchBar.sendKeys("Elon Musk"); //unosenje pojma u trazilicu
         WebElement searchButton = webDriver.findElement(By.xpath("//form[@id='global-nav-search']//button[@type='submit']"));
         searchButton.click(); //pretrazivanje
-        Thread.sleep(1500);
+        Thread.sleep(7500);
         webDriver.quit();
     }
 
-    @Test void testTweetsAndReplies() throws  InterruptedException {
-        testSearch(); // logiranje
+    @Test void testTweetsAndRepliesAndMedia() throws  InterruptedException {
+        testSearch(); // prijava
 
         WebElement tweetsAndReplies = webDriver.findElement(By.xpath("//*[@id=\"page-container\"]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[2]/ul/li[2]"));
         tweetsAndReplies.click(); //prijelaz na twittove i odgovore
@@ -119,17 +119,21 @@ public void setupTest() {
     }
 
     @Test void testMessage() throws InterruptedException {
-        testLogin(); // logiranje
+        testLogin(); // prijava
 
         WebElement message = webDriver.findElement(By.xpath("//*[@id=\"global-actions\"]/li[3]/a"));
         message.click(); //prijelaz na poruke
+        Thread.sleep(7500);
+        webDriver.quit();
     }
 
     @Test void testNotifications() throws  InterruptedException{
-        testLogin(); // logiranje
+        testLogin(); // prijava
 
         WebElement notifications = webDriver.findElement(By.xpath("//*[@id=\"global-actions\"]/li[2]/a"));
         notifications.click(); //prijelaz na obavjesti
+        Thread.sleep(7500);
+        webDriver.quit();
     }
 
 
